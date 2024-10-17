f52b58c37367c0b5321c70b61c5ffe07253b2161
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2024-10-17 15:55:47 +0400

    prepare the solution

---
+++ CS/TreeListBatchEditing/Data/CitiesContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TreeListBatchEditing.Services;
+
+namespace TreeListBatchEditing.Data {
+    public class CitiesContext : DbContext {
+        public DbSet<Location> Locations { get; set; }
+        public CitiesContext(DbContextOptions<CitiesContext> options) : base(options) { }
+    }
+}
---
+++ CS/TreeListBatchEditing/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using TreeListBatchEditing.Components;
+using TreeListBatchEditing.Services;
+using TreeListBatchEditing.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDevExpressBlazor(options => {
+    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
+});
+builder.Services.AddScoped<CitiesService>();
+builder.Services.AddDbContextFactory<CitiesContext>(opt =>
+        opt.UseSqlite($"Data Source=Cities.db"), ServiceLifetime.Scoped);
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope()) {
+    var citiesService = scope.ServiceProvider.GetRequiredService<CitiesService>();
+    citiesService.SeedData();
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
