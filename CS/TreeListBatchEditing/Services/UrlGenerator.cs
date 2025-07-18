b81a870421d0da1591aab28d8e96cf1a1877bab3
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2025-07-18 15:58:54 +0400

    update to v25.1

--- CS/TreeListBatchEditing/Services/UrlGenerator.cs
+++ CS/TreeListBatchEditing/Services/UrlGenerator.cs
@@ -1,8 +1,17 @@
-namespace TreeListBatchEditing {
+using System.Web;
+
+namespace TreeListBatchEditing.Services {
     public static class UrlGenerator {
         public const string ToggleSidebarName = "toggledSidebar";
         public static string GetUrl(string baseUrl, bool toggledSidebar) {
             return $"{baseUrl}?{ToggleSidebarName}={toggledSidebar}";
+        }
+        public static string GetUrl(bool toggledSidebar, string returnUrl) {
+            var baseUriBuilder = new UriBuilder(returnUrl);
+            var query = HttpUtility.ParseQueryString(baseUriBuilder.Query);
+            var baseUrl = baseUriBuilder.Fragment + baseUriBuilder.Host + baseUriBuilder.Path;
+
+            return $"{baseUrl}?{ToggleSidebarName}={toggledSidebar}&{query}";
         }
     }
 }
