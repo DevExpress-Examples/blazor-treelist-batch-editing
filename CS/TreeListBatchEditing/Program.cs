9e4babdf036f9ea6e329985cb819e63a199278d0
Elena Peskova <116714417+elenapeskova@users.noreply.github.com>
2025-09-17 17:57:16 +0400

    Remove BootstrapVersion from DevExpressBlazor options
    Removed BootstrapVersion setting from DevExpressBlazor options.

--- CS/TreeListBatchEditing/Program.cs
+++ CS/TreeListBatchEditing/Program.cs
@@ -10,7 +10,6 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddDevExpressBlazor(options => {
-    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
     options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
 });
 builder.Services.AddScoped<CitiesService>();
